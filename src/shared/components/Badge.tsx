ec1ec398978f8f2fe9ecb16119ac22d66fcfc5dc
Oussama Boussouf <oussamaboussouf06@gmail.com>
2025-10-17 21:13:27 +0100

    change the accent-color of checkbox and radio plus the background color for slider, page button and badge

--- src/shared/components/Badge.tsx
+++ src/shared/components/Badge.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 function Badge({ children }: { children: ReactNode }) {
-  return <span className="badge">{children}</span>;
+  return <span className="badge badge--primary fs-xxs">{children}</span>;
 }
 
 export default Badge;
