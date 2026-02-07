8325f2bc54b9dff29fb7db8f7cf6790a5aef4d05
Oussama Boussouf <oussamaboussouf06@gmail.com>
2026-02-07 22:35:08 +0100

    style(dashboard): sidebar for mobile view - Add side bar for the mobile view - Add gradient color to active link

--- src/layouts/DashboardSidebar.tsx
+++ src/layouts/DashboardSidebar.tsx
@@ -3,9 +3,42 @@ import { Link, NavLink } from 'react-router';
 import { useAuthContext } from '../context/AuthContext';
 import { ReactNode } from 'react';
 import { sidebarLinks } from '../constants/dashboardLinks';
+import { useMediaQuery } from '../shared/hooks/useMediaQuery';
 
 function DashboardSidebar() {
   const { user, logout } = useAuthContext();
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
+  if (isMobile) {
+    return (
+      <aside className="mobile-menu">
+        <div className="mobile-menu__logo-wrapper d-flex-center">
+          <Link to="/" className="mobile-menu__logo">
+            D
+          </Link>
+        </div>
+        <div className="seprater" />
+        <div className='mobile-menu__nav-container'>
+          <nav>
+            <ul className="mobile-menu__list">
+              {sidebarLinks[user!.role].map(
+                (link: { label: string; path: string; icon: ReactNode }) => (
+                  <li key={link.label} className='d-flex-center'>
+                    <NavLink to={link.path} className="mobile-menu__link d-flex-center">
+                      {link.icon}
+                    </NavLink>
+                  </li>
+                )
+              )}
+            </ul>
+          </nav>
+          <button onClick={logout} type="button" className="">
+            <LogOut color="red"/>
+          </button>
+        </div>
+      </aside>
+    );
+  }
 
   return (
     <aside className="dashboard__sidebar">
